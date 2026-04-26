f596fd2f0e069b50bfa9f808841471ebde5ea26c
Erik <mayilyane9@gmail.com>
2026-04-26 19:51:12 +0200

    more changes

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Hero.css";
+
+const Hero = () => {
+  return (
+    <div id='hero' className='hero'>
+      <h1>Your <span className='career'>career</span> starts here. Discover <span className='career'>oppotunities</span> made for students like you. </h1>
+    </div>
+  )
+}
+
+export default Hero
